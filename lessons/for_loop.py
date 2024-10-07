9604887d64eaba0e3de0d8d1648cae6dfe162759
chiemc <cchiem@unc.edu>
2024-10-07 09:52:40 -0400

    10/7/24 lecture: for loop and range practice

---
+++ lessons/for_loop.py
@@ -0,0 +1,8 @@
+"For loop practice"
+
+
+pets: list[str] = ["Louie", "Bo", "Bear"]
+
+# tell each pet they're a good boy (pause)
+for name in pets:
+    print(f"Good boy, {name}!")
---
+++ lessons/range_practice.py
@@ -0,0 +1,7 @@
+"""practice with ranges"""
+
+names: list[str] = ["Alyssa", "Janet", "Vrinda"]
+
+# print every element's index and value
+for idx in range(0, len(names)):
+    print(f"{idx}: {names[idx]}")
