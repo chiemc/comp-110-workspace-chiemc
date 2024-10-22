3ba24b299b3c11e097684907f642482902dede60
chiemc <cchiem@unc.edu>
2024-10-22 16:40:41 -0400

    prorgress

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -28,7 +28,12 @@ def sub(numbers: list[int], start_index: int, end_index: int) -> list[int]:
 def add_at_index(numbers: list[int], add_element: int, desired_index: int) -> None:
     if desired_index < 0 or desired_index > len(numbers):
         raise IndexError("Index is out of bounds for the input list")
-    numbers.append(add_element)
-    
+    numbers.append(100)
+    # placeholder to make space for element
+    for elem in range(len(numbers) - 1, desired_index, - 1):
+        # add - 1 after desired_index to signify to python to shift right
+        # shifts all elements above desired_index to make space for the new element
+        # start from the end of the list, shift right to higher indices
+        numbers[elem] = numbers[elem - 1]
     numbers[desired_index] = add_element
     return None
