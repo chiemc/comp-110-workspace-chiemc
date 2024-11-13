b3f03a4f85c0d820848978bb2f2cff60d4fa9afa
chiemc <cchiem@unc.edu>
2024-11-13 09:53:47 -0500

    linked list lesson

---
+++ lessons/linked_list.py
@@ -0,0 +1,55 @@
+"""Intro to Recursive Structures and Processes"""
+
+from __future__ import annotations
+
+
+class Node:
+    value: int
+    next: Node | None
+
+    def __init__(self, val: int, next: Node | None):
+        self.value = val
+        self.next = next
+
+    def __str__(self) -> str:
+        """Represent a Node object as a string."""
+        rest: str = "?"
+        if self.next is None:
+            rest = "None"
+        else:
+            rest = str(self.next)
+        # Wishful thinking: Get rest of list str
+        return f"{self.value} -> {rest}"
+
+
+two: Node = Node(2, None)
+one: Node = Node(1, two)
+courses: Node = Node(110, Node(210, Node(301, None)))
+print(one)
+print(courses)
+# Be sure to get to here!
+
+
+def to_str(head: Node | None) -> str:
+    """Represent a Linked List as a str."""
+    if head is None:
+        return "None"
+    else:
+        rest: str = to_str(head.next)
+        return f"{head.value} -> {rest}"
+
+
+print(to_str(one))
+print(to_str(courses))
+
+
+def last(head: Node) -> int:
+    """Return the last value of a Linked List."""
+    if head.next is None:
+        return head.value  # Base case!
+    else:
+        return last(head.next)  # Recursive case
+
+
+print(last(one))  # expect 2
+print(last(courses))  # expect 301
