e5f0cdeb1386c6dd3db0649bad36bb9726d739df
chiemc <cchiem@unc.edu>
2024-11-15 09:53:36 -0500

    11/15/24 linked_list recursion practice

--- lessons/linked_list.py
+++ lessons/linked_list.py
@@ -53,3 +53,26 @@ def last(head: Node) -> int:
 
 print(last(one))  # expect 2
 print(last(courses))  # expect 301
+
+
+def recursive_range(start: int, end: int) -> Node | None:
+    """Build a linked list recursively from start up until end (not inclusive)"""
+    # raise an exception "Invalid use of recursive_range" when called improperly
+    # edge case
+    if start < end:
+        raise ValueError("Invalid use of recursive_range")
+    if start == end:
+        return None
+    else:
+        # recursive case:
+        # intuition: handle the first case baesd on the specific call
+        # build the rest of the list using the function recursively
+        return Node(start, recursive_range(start + 1, end))
+
+
+# try declaring a variable and assigning it the result of recursive_range
+# then try printing that variable as a string
+a: Node | None = recursive_range(2, 8)
+b: Node | None = recursive_range(110, 112)
+print(a)
+print(b)
